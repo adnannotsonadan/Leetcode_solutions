5c80410346f4006b08ce15c4140a754edd5d7e00
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-01-11 16:03:58 +0530

    Time: 188 ms (63.23%), Space: 90.2 MB (24%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
@@ -9,18 +9,34 @@ class Solution(object):
         :type head: Optional[ListNode]
         :rtype: int
         """
-        all_sums=[]
+        # all_sums=[]
         
+        # arr=[]
+        # temp=head
+        # while temp:
+        #     arr.append(temp.val)
+        #     temp=temp.next
+        
+        # n = len(arr)
+        # max_sum = 0
+        
+        # for i in range(n//2):
+        #     max_sum = max(max_sum, arr[i] + arr[n-1-i])
+        
+        # return max_sum
+# approach2
         arr=[]
+        n=0
+        m=0
         temp=head
         while temp:
             arr.append(temp.val)
+            n+=1
             temp=temp.next
-        
-        n = len(arr)
-        max_sum = 0
-        
-        for i in range(n//2):
-            max_sum = max(max_sum, arr[i] + arr[n-1-i])
-        
-        return max_sum
+        i=0
+        j=n-1
+        while i<j:
+            m=max(m,arr[i]+arr[j])
+            j-=1
+            i+=1
+        return m
