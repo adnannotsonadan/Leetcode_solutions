6c6fd77e098fd35c280abc73a3a877831c4c7051
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-01-14 02:52:17 +0530

    Time: 307 ms (34.48%), Space: 0B (100%) - LeetHub

---
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,18 @@
+# Write your MySQL query statement below
+    SELECT
+        id,
+        SUM(num) AS num
+    FROM (
+        SELECT accepter_id AS id, COUNT(*) AS num
+        FROM RequestAccepted
+        GROUP BY accepter_id
+
+        UNION ALL
+
+        SELECT requester_id AS id, COUNT(*) AS num
+        FROM RequestAccepted
+        GROUP BY requester_id
+    ) t
+    GROUP BY id
+    ORDER BY num DESC
+    LIMIT 1;
