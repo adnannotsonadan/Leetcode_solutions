8760d2f6c349ec74d6f8a63a96a91cec538a1385
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-01-13 08:44:49 +0530

    Time: 0 ms (100%), Space: 12.7 MB (34.94%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -4,20 +4,40 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        stack = []  
+        # stack = []  
 
-        for ch in s:
-            if ch in "({[":
-                stack.append(ch)  
-            else:
-                if not stack:
-                    return False  
-                top = stack.pop()
+        # for ch in s:
+        #     if ch in "({[":
+        #         stack.append(ch)  
+        #     else:
+        #         if not stack:
+        #             return False  
+        #         top = stack.pop()
 
                 
-                if (ch == ')' and top == '(') or (ch == ']' and top == '[') or (ch == '}' and top == '{'):
-                    continue
+        #         if (ch == ')' and top == '(') or (ch == ']' and top == '[') or (ch == '}' and top == '{'):
+        #             continue
+        #         else:
+        #             return False
+
+        # return len(stack) == 0
+        stack=[]
+        for x in s:
+            if x in "([{":
+                stack.append(x)
+            else:
+                if not stack:
+                    return False
+                    
+                if x==')' and stack[-1]=='(' or x==']' and stack[-1]=='[' or x=='}' and stack[-1]=='{':
+                    stack.pop()  
                 else:
                     return False
-
-        return len(stack) == 0
+                    
+        if len(stack)==0:
+            return True
+        
+        else:
+            return False
+            
+            
