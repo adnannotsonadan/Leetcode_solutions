bb6d4f610d584d1ba1784f72daf7dfb3b4790509
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-01-15 02:24:30 +0530

    Time: 1137 ms (74.79%), Space: 0B (100%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,6 +1,11 @@
 -- Write your PostgreSQL query statement below
 
-SELECT p.product_name, year, price
-FROM Sales
-JOIN Product p
-    USING (product_id);
+-- SELECT p.product_name, year, price
+-- FROM Sales
+-- JOIN Product p
+--     USING (product_id);
+
+select Product.product_name,Sales.year,Sales.price
+from Product
+right join Sales
+on Product.product_id=Sales.product_id;
