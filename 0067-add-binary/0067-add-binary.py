79a748cf75a39ab32b6c6b396c8d7d5ccc06c6b9
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-01-15 11:39:12 +0530

    Time: 1 ms (66.59%), Space: 12.5 MB (56.68%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        x = int(a,2)
+        y = int(b,2)
+        return str(bin(x+y).replace("0b",""))
