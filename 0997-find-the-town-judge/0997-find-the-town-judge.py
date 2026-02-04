d2a9b242e170007e70d628b8fd3328e7c9dc66fd
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-02-04 15:58:50 +0530

    Time: 96 ms (7.02%), Space: 23.8 MB (7.02%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -5,6 +5,7 @@ class Solution(object):
         :type trust: List[List[int]]
         :rtype: int
         """
+        # sol1
         # lst=[[]for _ in range(n+1)]
         # indeg=[0]*(n+1)
         # for u,v in trust:
@@ -14,15 +15,28 @@ class Solution(object):
         #     if len(lst[i])==0 and indeg[i]==n-1:
         #         return i
         # return -1
-        lst=[[] for _ in range(n+1)]
+
+        # sol2
+        # lst=[[] for _ in range(n+1)]
+        # indeg=[0]*(n+1)
+        # outdeg=[0]*(n+1)
+        # for u,v in trust:
+        #     lst[u].append(v)
+        #     indeg[v]+=1
+        # for i in range(1,n+1):
+        #     outdeg[i]=len(lst[i])
+        # for i in range(1,n+1):
+        #     if indeg[i]==n-1 and outdeg[i]==0:
+        #         return i
+        # return -1
+
+        # sol3
+        mat=[[0]*(n+1) for _ in range(n+1)]
         indeg=[0]*(n+1)
-        outdeg=[0]*(n+1)
         for u,v in trust:
-            lst[u].append(v)
+            mat[u][v]=1
             indeg[v]+=1
         for i in range(1,n+1):
-            outdeg[i]=len(lst[i])
-        for i in range(1,n+1):
-            if indeg[i]==n-1 and outdeg[i]==0:
+            if sum(mat[i])==0 and indeg[i]==(n-1):
                 return i
         return -1
