d128fc9f2b0fb0ea02e84a5080e0fbaa93f3e74a
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-02-10 20:23:35 +0530

    Time: 273 ms (21.55%), Space: 16.9 MB (17.3%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -6,24 +6,28 @@ class Solution(object):
         """
         n=len(mat)
         m=len(mat[0])
-        moves=[[-1,0],[1,0],[0,-1],[0,1]]
-        dist=[[-1]*m for _ in range(n)]
-        q=[]
-        for i in range(n):
-            for j in range(m):
-                if mat[i][j]==0:
-                    q.append((i,j))
-                    dist[i][j]=0
-        while q:
-            f1,f2=q[0]
-            q.pop(0)
+        moves=[(-1,0),(1,0),(0,-1),(0,1)]
 
-            for dx,dy in moves:
-                ni=f1+dx
-                nj=f2+dy
-                if ni>=0 and ni<n and nj>=0 and nj<m:
-                    if dist[ni][nj]==-1:
-                        dist[ni][nj]=dist[f1][f2]+1
-                        q.append((ni,nj))
-        return dist
+        def bfs():
+            q=[]
+            vis=[[0]*m for _ in range(n)]
+            dist=[[-1]*m for _ in range(n)]
+            for i in range(n):
+                for j in range(m):
+                    if mat[i][j]==0:
+                        dist[i][j]=0
+                        vis[i][j]=1
+                        q.append((i,j))
+            while q:
+                f1,f2=q.pop(0)
+                for dx,dy in moves:
+                    ni=f1+dx
+                    nj=f2+dy
+                    if ni>=0 and nj>=0 and ni<n and nj<m:
+                        if vis[ni][nj]==0:
+                            dist[ni][nj]=dist[f1][f2]+1
+                            q.append((ni,nj))
+                            vis[ni][nj]=1
+            return dist
+        return bfs()
             
