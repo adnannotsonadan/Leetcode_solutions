988b7111110bafe70d3dcf475c08cdec8b3249b9
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-02-14 09:23:07 +0530

    Time: 0 ms (100%), Space: 12.7 MB (21.26%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,16 +1,15 @@
 class Solution(object):
     def climbStairs(self, n):
-        if n <= 1:
-            return 1
-        
-        dp = [-1] * (n + 1)
-        return self.climbStairsHelper(n, dp)
-    
-    def climbStairsHelper(self, n, dp):
-        if n <= 1:
-            return 1
-        if dp[n] != -1:
-            return dp[n]
-        
-        dp[n] = self.climbStairsHelper(n - 1, dp) + self.climbStairsHelper(n - 2, dp)
-        return dp[n]
+        dp=[-1]*(n+1)
+        def rec(ind):
+            if ind==0:
+                return 1
+            if ind==1:
+                return 1
+            if dp[ind]!=-1:
+                return dp[ind]
+            left=rec(ind-1)
+            right=rec(ind-2)
+            dp[ind] = right+left 
+            return dp[ind]
+        return rec(n)
