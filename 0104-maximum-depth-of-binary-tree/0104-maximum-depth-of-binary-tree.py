7fab5d649525c7f6da34df88cb5ee9e36a61d19e
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-03-10 22:30:33 +0530

    Time: 3 ms (62.59%), Space: 15.1 MB (40.5%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -10,21 +10,11 @@ class Solution(object):
         :type root: Optional[TreeNode]
         :rtype: int
         """
-        if not root:
-            return 0
-        dep=0
-        def lvl(node,dep):
-            q=deque()
-            q.append(node)
-            while q:
-                dep+=1
-                for _ in range(len(q)):
-                    f=q.popleft()
-                    if f.left is not None:
-                        q.append(f.left)
-                    if f.right is not None:
-                        q.append(f.right)
-            return dep
-        res=lvl(root,dep)
-        return res
         
+        def dep(node):
+            if not node:
+                return 0
+            lef=dep(node.left)
+            righ=dep(node.right)
+            return 1+max(lef,righ)
+        return dep(root)
