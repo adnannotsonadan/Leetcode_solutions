25e8ecc5d40875fbe161391ad9a503bd01ec2180
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-03-11 23:48:36 +0530

    Time: 0 ms (100%), Space: 12.6 MB (56.89%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -1,11 +1,24 @@
-class Solution:
-    def isTreeSymmetric(self, leftRoot, rightRoot):
-        if leftRoot is None and rightRoot is None:
-            return True
-        if (leftRoot is None and rightRoot is not None) or (leftRoot is not None and rightRoot is None):
-            return False
-        if leftRoot.val != rightRoot.val:
-            return False
-        return self.isTreeSymmetric(leftRoot.left, rightRoot.right) and self.isTreeSymmetric(leftRoot.right, rightRoot.left)
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
     def isSymmetric(self, root):
-        return self.isTreeSymmetric(root.left, root.right)
+        """
+        :type root: Optional[TreeNode]
+        :rtype: bool
+        """
+        
+        def lvlOrder(left,right):
+            if left==None and right==None:
+                return True
+            if left==None or right==None:
+                return False
+            if left.val!=right.val:
+                return False
+            l=lvlOrder(left.left,right.right)
+            r=lvlOrder(left.right,right.left)
+            return l and r
+        return lvlOrder(root.left,root.right)
