0b2079d9ebf18a4d85fe2457810504856ff0613c
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-03-11 07:44:52 +0530

    Time: 4 ms (65.7%), Space: 17.9 MB (73.39%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -1,21 +1,28 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-class Solution:
-    # @param {TreeNode} root
-    # @return {boolean}
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
     def isBalanced(self, root):
-        if not root:
-            return True
-
-        return abs(self.getHeight(root.left) - self.getHeight(root.right)) < 2 and self.isBalanced(root.left) and self.isBalanced(root.right)
-
-    def getHeight(self, root):
-        if not root:
-            return 0
-
-        return 1 + max(self.getHeight(root.left), self.getHeight(root.right))
+        """
+        :type root: Optional[TreeNode]
+        :rtype: bool
+        """
+        def dep(node):
+            if not node:
+                return 0
+            lf=dep(node.left)
+            if lf==-1:
+                return -1
+            rg=dep(node.right)
+            if rg==-1:
+                return -1
+            if abs(lf-rg)>1:
+                return -1
+            return 1 + max(lf,rg)
+        x = dep(root)
+        if x==-1:
+            return False
+        return True
