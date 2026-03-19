867b188c1b9f1cb9ac53196d324b2aefb15a095e
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-03-20 04:51:30 +0530

    Time: 12 ms (33.79%), Space: 13.2 MB (56.15%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,57 +1,28 @@
 class Solution(object):
     def findCircleNum(self, isConnected):
-        """
-        :type isConnected: List[List[int]]
-        :rtype: int
-        """
-        # n=len(isConnected)
-        # adj=[[] for _ in range(n)]
-        # for i in range(n):
-        #     for j in range(n):
-        #         if isConnected[i][j]==1:
-        #             adj[i].append(j)
-        #             adj[j].append(i)
-        # def dfs(i):
-        #     vis[i]=1
-        #     for nei in adj[i]:
-        #         if vis[nei]==0:
-        #             dfs(nei)
-            
-        # vis=[0]*(n)
-        # l=0
-        # for i in range(n):
-        #     if vis[i]==0:
-        #         l+=1
-        #         dfs(i)
-        # return l
-        n=len(isConnected)
-        
-        dsu=DSU(n)
+        n = len(isConnected)
+
+        #  matrix tp adj list conv
+        adj = [[] for _ in range(n)]
         for i in range(n):
             for j in range(n):
-                if isConnected[i][j]==1:
-                    dsu.unite(i,j)
-        return dsu.components
+                if isConnected[i][j] == 1 and i != j:
+                    adj[i].append(j)
 
-class DSU:
-    def __init__(self,n):
-        self.parent=[i for i in range(n)]
-        self.size=[1]*(n)
-        self.components=n
-    def find(self,x):
-        if x==self.parent[x]:
-            return x
-        self.parent[x]=self.find(self.parent[x])
-        return self.parent[x]
-    def unite(self,u,v):
-        u=self.find(u)
-        v=self.find(v)
+        
+        vis = [0] * n
 
-        if u==v:
-            return False
-        if self.size[u]<self.size[v]:
-            u,v=v,u
-        self.parent[v]=u
-        self.size[u]+=self.size[v]
-        self.components-=1
-        return True
+        def dfs(i):
+            vis[i] = 1
+            for nei in adj[i]:
+                if vis[nei] == 0:
+                    dfs(nei)
+
+        
+        cc = 0
+        for i in range(n):
+            if vis[i] == 0:
+                cc += 1
+                dfs(i)
+
+        return cc
