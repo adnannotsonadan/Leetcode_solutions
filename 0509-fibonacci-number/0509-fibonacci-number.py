25315d6b3e1b21b9547dca83d1b0bacf93f22223
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-03-22 16:54:28 +0530

    Time: 20 ms (49.45%), Space: 12.4 MB (42.3%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n<=1:
+            return n
+        dp=[-1]*(n+1)
+        dp[0]=0
+        dp[1]=1
+
+        for i in range(2,n+1):
+            dp[i]=dp[i-1]+dp[i-2]
+        return dp[n]
