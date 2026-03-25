7e6a5e98f8502350a00f4c7c022ed9ba4d499045
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-03-25 10:02:00 +0530

    Time: 6 ms (46.1%), Space: 18 MB (56.08%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -10,34 +10,21 @@ class Solution(object):
         :type root: Optional[TreeNode]
         :rtype: bool
         """
-        # def dep(node):
-        #     if not node:
-        #         return 0
-        #     lf=dep(node.left)
-        #     if lf==-1:
-        #         return -1
-        #     rg=dep(node.right)
-        #     if rg==-1:
-        #         return -1
-        #     if abs(lf-rg)>1:
-        #         return -1
-        #     return 1 + max(lf,rg)
-        # x = dep(root)
-        # if x==-1:
-        #     return False
-        # return True
+        if not root:
+            return True
         def dep(node):
             if not node:
                 return 0
-            l=dep(node.left)
-            if l==-1:
+            left=dep(node.left)
+            if left==-1:
                 return -1
-            r=dep(node.right)
-            if r==-1:
+            right=dep(node.right)
+            if right==-1:
                 return -1
-            if abs(l-r)>1:
+            if abs(left-right)>1:
                 return -1
-            return 1+max(l,r)
+            return 1+max(left,right)
         if dep(root)!=-1:
             return True
         return False
+        
