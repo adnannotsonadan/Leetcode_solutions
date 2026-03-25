a46dfedccd398d2894b3d43a273b4354497b7eb4
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-03-25 11:07:01 +0530

    Time: 0 ms (100%), Space: 12.7 MB (67.55%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
@@ -4,34 +4,48 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
 class Solution(object):
     def zigzagLevelOrder(self, root):
         """
         :type root: Optional[TreeNode]
         :rtype: List[List[int]]
         """
+        # if not root:
+        #     return []
+        # q=deque([root])
+        # res=[]
+        # while q:
+        #     lvl=[]
+        #     for i in range(len(q)):
+        #         f=q.popleft()
+        #         if not f:
+        #             continue
+        #         lvl.append(f.val)
+        #         if f.left:
+        #             q.append(f.left)
+        #         if f.right:
+        #             q.append(f.right)
+        #     res.append(lvl[:])
+        
+        # return res
         res=[]
         if not root:
             return []
-        def lvl(node):
-            l_to_r=True
-            q=deque([root])
-            while q:
-                lvl_size=len(q)
-                curr_lvl=[]
-                for _ in range(lvl_size):
-                    f=q.popleft()
-                    curr_lvl.append(f.val)
-                    if f.left is not None:
-                        q.append(f.left)
-                    if f.right is not None:
-                        q.append(f.right)
-                if l_to_r:
-                    res.append(curr_lvl)
-                    l_to_r=False
-                elif not l_to_r:
-                    res.append(curr_lvl[::-1])
-                    l_to_r=True
-        lvl(root)
+        q=deque([root])
+        trav=True
+        while q:
+            lvl=[]
+            for i in range(len(q)):
+                node=q.popleft()
+                lvl.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            if trav:
+                res.append(lvl[:])
+            else:
+                res.append(lvl[::-1])
+            trav=not trav
         return res
-        
