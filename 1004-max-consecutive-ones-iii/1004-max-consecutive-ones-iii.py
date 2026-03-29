0c7446d98cb3287cb736cf292554a8ae4d30056f
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-03-29 10:55:14 +0530

    Time: 113 ms (19.74%), Space: 13.6 MB (92.8%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -23,19 +23,35 @@ class Solution(object):
 
 
         # OPTIMAL
+        # l=0
+        # r=0
+        # n=len(nums)
+        # zeros=0
+        # maxi=0
+        # while r<n:
+        #     if nums[r]==0:
+        #         zeros+=1
+        #     while zeros>k:
+        #         if nums[l]==0:
+        #             zeros-=1
+        #         l+=1
+        #     if zeros<=k:
+        #         maxi=max(maxi,r-l+1)
+        #     r+=1
+        # return maxi
+
         l=0
         r=0
-        n=len(nums)
-        zeros=0
         maxi=0
+        n=len(nums)
+        z=0
         while r<n:
             if nums[r]==0:
-                zeros+=1
-            while zeros>k:
+                z+=1
+            while z>k:
                 if nums[l]==0:
-                    zeros-=1
+                    z-=1
                 l+=1
-            if zeros<=k:
-                maxi=max(maxi,r-l+1)
+            maxi=max(maxi,r-l+1)
             r+=1
         return maxi
