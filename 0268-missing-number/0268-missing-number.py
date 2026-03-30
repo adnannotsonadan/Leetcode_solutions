3a6b5e1d690db2f16097b10c3175c86b6c90f172
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-03-30 12:06:59 +0530

    Time: 0 ms (100%), Space: 13.2 MB (97.12%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -4,9 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        m=max(nums)
-        nums.sort()
-        for i in range(m+1):
-            if i!=nums[i]:
-                return i
-        return m+1
+        # m=max(nums)
+        # nums.sort()
+        # for i in range(m+1):
+        #     if i!=nums[i]:
+        #         return i
+        # return m+1
+
+        n=len(nums)
+        n1=(n*(n+1))//2
+        s=sum(nums)
+
+        d=n1-s
+        return d
