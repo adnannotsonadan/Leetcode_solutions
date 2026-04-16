10c39cb0c87441cf0d42d1df72f87497aaa9cb5c
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-04-16 23:23:38 +0530

    Time: 103 ms (40.61%), Space: 19.5 MB (16.79%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        # BRUTE FORCE
+        # maxi=0
+        # for i in range(len(prices)):
+        #     for j in range(i+1,len(prices)):
+        #         if prices[j]>prices[i]:
+        #             x=prices[j]-prices[i]
+        #             maxi=max(maxi,x)
+        # return maxi
+
+        # OPTIMAL
+        maxi=0
+        mini=float('inf')
+
+        for i in range(len(prices)):
+            if prices[i]<mini:
+                mini=prices[i]
+            if prices[i]>mini:
+                maxi=max(maxi,prices[i]-mini)
+        return maxi
