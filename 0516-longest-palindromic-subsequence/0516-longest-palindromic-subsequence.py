85eafceef99fd74fe119e417a3005fb124a8dbaa
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-04-19 13:55:11 +0530

    Time: 1867 ms (18.3%), Space: 33.7 MB (23.23%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
@@ -45,18 +45,30 @@ class Solution(object):
         #         pal.append(x)
         # return max(len(x) for x in pal)
 
+        # n=len(s)
+        # dp=[[-1]*(n+1) for _ in range(n+1)]
+        # def rec(i,j):
+        #     if i<0 or j<0:
+        #         return 0
+        #     if dp[i][j]!=-1:
+        #         return dp[i][j]
+        #     if s[i]==r[j]:
+        #         dp[i][j]=1+rec(i-1,j-1)
+        #         return dp[i][j]
+        #     else:
+        #         dp[i][j]=max(rec(i-1,j),rec(i,j-1))
+        #         return dp[i][j]
+        # return rec(n-1,n-1)
+        
+        
         n=len(s)
-        dp=[[-1]*(n+1) for _ in range(n+1)]
         r=s[::-1]
-        def rec(i,j):
-            if i<0 or j<0:
-                return 0
-            if dp[i][j]!=-1:
-                return dp[i][j]
-            if s[i]==r[j]:
-                dp[i][j]=1+rec(i-1,j-1)
-                return dp[i][j]
-            else:
-                dp[i][j]=max(rec(i-1,j),rec(i,j-1))
-                return dp[i][j]
-        return rec(n-1,n-1)
+        dp=[[0]*(n+1) for _ in range(n+1)]
+
+        for i in range(1,n+1):
+            for j in range(1,n+1):
+                if s[i-1]==r[j-1]:
+                    dp[i][j]=1+dp[i-1][j-1]
+                else:
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+        return dp[n][n]
