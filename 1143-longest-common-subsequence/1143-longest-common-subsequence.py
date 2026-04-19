3b92ecee7517d268d145b1b5b3520aa58c29c5cf
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-04-19 11:40:04 +0530

    Time: 548 ms (68.25%), Space: 33.6 MB (52.4%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -5,17 +5,30 @@ class Solution(object):
         :type text2: str
         :rtype: int
         """
+        # n=len(text1)
+        # m=len(text2)
+        # dp=[[-1]*(m+1) for _ in range(n+1)]
+        # def rec(ind1,ind2):
+        #     if ind1==0 or ind2==0:
+        #         return 0
+        #     if dp[ind1][ind2]!=-1:
+        #         return dp[ind1][ind2]
+        #     if text1[ind1-1]==text2[ind2-1]:
+        #         dp[ind1][ind2]=1+rec(ind1-1,ind2-1)
+        #     else:
+        #         dp[ind1][ind2]=max(rec(ind1-1,ind2),rec(ind1,ind2-1))
+        #     return dp[ind1][ind2]
+        # return rec(n,m)
+        
+        
         n=len(text1)
         m=len(text2)
-        dp=[[-1]*m for _ in range(n)]
-        def rec(ind1,ind2):
-            if ind1<0 or ind2<0:
-                return 0
-            if dp[ind1][ind2]!=-1:
-                return dp[ind1][ind2]
-            if text1[ind1]==text2[ind2]:
-                dp[ind1][ind2]=1+rec(ind1-1,ind2-1)
-            if text1[ind1]!=text2[ind2]:
-                dp[ind1][ind2]=max(rec(ind1-1,ind2),rec(ind1,ind2-1))
-            return dp[ind1][ind2]
-        return rec(n-1,m-1)
+        dp=[[0]*(m+1) for _ in range(n+1)]
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if text1[i-1]==text2[j-1]:
+                    dp[i][j]=1+dp[i-1][j-1]
+                else:
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+                
+        return dp[n][m]
