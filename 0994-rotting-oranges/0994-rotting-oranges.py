379b77ad0ddb8cbdab7ae3bafd4f7ffafc00bb9f
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-04-20 13:28:18 +0530

    Time: 5 ms (59.07%), Space: 12.3 MB (64.15%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,3 +1,4 @@
+from collections import deque
 class Solution(object):
     def orangesRotting(self, grid):
         """
@@ -6,32 +7,28 @@ class Solution(object):
         """
         n=len(grid)
         m=len(grid[0])
-        moves=[(-1,0),(1,0),(0,-1),(0,1)]
         vis=[[0]*m for _ in range(n)]
-        def bfs(si,sj,t):
-            q=[]
-            for i in range(n):
-                for j in range(m):
-                    if grid[i][j]==2:
-                        # vis[i][j]=2
-                        q.append((i,j,0))
-
-            tm=0
-            while q:
-                f1,f2,t=q.pop(0)
-                tm=max(tm,t)
-                for dx,dy in moves:
-                    ni=dx+f1
-                    nj=dy+f2
-                    if ni>=0 and ni<n and nj>=0 and nj<m and grid[ni][nj]==1:
-                        if vis[ni][nj]==0: 
-                            vis[ni][nj]=1
-                            grid[ni][nj]=2
-                            q.append((ni,nj,t+1))
-            for i in range(n):
-                for j in range(m):
-                    if grid[i][j]==1:
-                        return -1
-            return tm
-        return bfs(0,0,0)
-            
+        tm=0
+        moves=[(1,0),(0,1),(-1,0),(0,-1)]
+        q=deque()
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j]==2:
+                    q.append((i,j,0))    
+        
+        while q:
+            f1,f2,t=q.popleft()
+            tm=max(tm,t)
+            for di,dj in moves:
+                ni=f1+di
+                nj=f2+dj
+                if ni>=0 and ni<n and nj>=0 and nj<m and grid[ni][nj]==1:
+                    if vis[ni][nj]==0:
+                        vis[ni][nj]=1
+                        grid[ni][nj]=2
+                        q.append((ni,nj,t+1))
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j]==1:
+                    return -1
+        return tm
