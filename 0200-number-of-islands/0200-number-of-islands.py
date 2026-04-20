e19d05b30b10c014a68be9d2bbc85de06cbd9264
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-04-20 17:11:10 +0530

    Time: 232 ms (47.25%), Space: 29.9 MB (22.28%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -37,36 +37,62 @@ class Solution(object):
         # return islands
 
 # with visited
-        n = len(grid)
-        m = len(grid[0])
+        # n = len(grid)
+        # m = len(grid[0])
 
-        visited = [[0] * m for _ in range(n)]
+        # visited = [[0] * m for _ in range(n)]
 
-        def dfs(i, j):
-            visited[i][j] = 1
+        # def dfs(i, j):
+        #     visited[i][j] = 1
 
-            # up
-            if i > 0 and grid[i-1][j] == "1" and visited[i-1][j] == 0:
-                dfs(i-1, j)
+        #     # up
+        #     if i > 0 and grid[i-1][j] == "1" and visited[i-1][j] == 0:
+        #         dfs(i-1, j)
 
-            # down
-            if i < n-1 and grid[i+1][j] == "1" and visited[i+1][j] == 0:
-                dfs(i+1, j)
+        #     # down
+        #     if i < n-1 and grid[i+1][j] == "1" and visited[i+1][j] == 0:
+        #         dfs(i+1, j)
 
-            # left
-            if j > 0 and grid[i][j-1] == "1" and visited[i][j-1] == 0:
-                dfs(i, j-1)
+        #     # left
+        #     if j > 0 and grid[i][j-1] == "1" and visited[i][j-1] == 0:
+        #         dfs(i, j-1)
 
-            # right
-            if j < m-1 and grid[i][j+1] == "1" and visited[i][j+1] == 0:
-                dfs(i, j+1)
+        #     # right
+        #     if j < m-1 and grid[i][j+1] == "1" and visited[i][j+1] == 0:
+        #         dfs(i, j+1)
 
-        islands = 0
+        # islands = 0
 
+        # for i in range(n):
+        #     for j in range(m):
+        #         if grid[i][j] == "1" and visited[i][j] == 0:
+        #             dfs(i, j)
+        #             islands += 1
+
+        # return islands
+
+
+
+
+
+
+
+
+        n=len(grid)
+        m=len(grid[0])
+        vis=[[0]*m for _ in range(n)]
+        moves=[(1,0),(0,1),(-1,0),(0,-1)]
+        def dfs(i,j):
+            vis[i][j]=1
+            for di,dj in moves:
+                ni=di+i
+                nj=dj+j
+                if ni>=0 and ni<n and nj>=0 and nj<m and vis[ni][nj]==0 and grid[ni][nj]=='1':
+                    dfs(ni,nj)
+        cc=0
         for i in range(n):
             for j in range(m):
-                if grid[i][j] == "1" and visited[i][j] == 0:
-                    dfs(i, j)
-                    islands += 1
-
-        return islands
+                if grid[i][j]=='1' and vis[i][j]==0:
+                    cc+=1
+                    dfs(i,j)
+        return cc
