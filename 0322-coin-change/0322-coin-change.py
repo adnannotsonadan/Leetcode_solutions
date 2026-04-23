66d0ed0a17b9281026eb660d200c57403bd3df19
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-04-23 23:25:54 +0530

    Time: 1604 ms (10.96%), Space: 37.3 MB (8.78%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -6,47 +6,59 @@ class Solution(object):
         :rtype: int
         """
         # n=len(coins)
-        # dp=[[-1]*(amount+1) for _ in range(n)]
-        # def rec(ind,amount):
-        #     if amount==0:
-        #         return 0
-        #     if ind==0:
-        #         if amount%coins[0]==0:
-        #             return amount//coins[0]
+        # dp=[[0]*(amount+1) for _ in range(n+1)]
+        # for i in range(n+1):
+        #     dp[i][0]=0
+        # for i in range(1,n+1):
+        #     for j in range(1,amount+1):
+        #         if coins[i-1]<=j:
+        #             inc=1+dp[i][j-coins[i-1]]
+        #             ninc=dp[i-1][j]
+        #             dp[i][j]=min(inc,ninc)
         #         else:
-        #             return float('inf')
-        #     if dp[ind][amount]!=-1:
-        #         return dp[ind][amount]
-        #     np=0+rec(ind-1,amount)
-        #     take=float('inf')
-        #     if coins[ind]<=amount:
-        #         take=1+rec(ind,amount-coins[ind])
-        #     dp[ind][amount]=min(np,take)
-        #     return dp[ind][amount]
-        # ans=rec(n-1,amount)
-        # if ans==float('inf'):
-        #     return -1
-        # return ans
+        #             dp[i][j]=dp[i-1][j]
+        # return dp[n][amount]
+
+
+
+
+
+
+
 
 
         n=len(coins)
-        dp=[[0]*(amount+1) for _ in range(n)]
-
-        for i in range(0,amount+1):
-            if i%coins[0]==0:
-                dp[0][i]=i//coins[0]
-            else:
-                dp[0][i]=float('inf')
-            
-        for ind in range(1,n):
-            for amt in range(amount+1):
-                np=0+dp[ind-1][amt]
-                p=float('inf')
-                if coins[ind]<=amt:
-                    p=1+dp[ind][amt-coins[ind]]
-                dp[ind][amt]=min(p,np)
-        ans=dp[n-1][amount]
+        dp=[[-1]*(amount+1) for _ in range(n)]
+        def rec(ind,amount):
+            if amount==0:
+                return 0
+            if ind==0:
+                if amount%coins[0]==0:
+                    return amount//coins[0]
+                return float('inf')
+            if dp[ind][amount]!=-1:
+                return dp[ind][amount]
+            np=rec(ind-1,amount)
+            pick=float('inf')
+            if coins[ind]<=amount:
+                pick=1+rec(ind,amount-coins[ind])
+            dp[ind][amount]=min(pick,np)
+            return dp[ind][amount]
+        ans=rec(n-1,amount)
         if ans==float('inf'):
             return -1
         return ans
-            
+
+
+
+
+
+
+
+
+
+
+
+
+
+             
