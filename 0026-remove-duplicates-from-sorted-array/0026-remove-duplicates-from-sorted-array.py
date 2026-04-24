5caee6039dbed3718aa6f242894afbb7c642e21a
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-04-24 21:55:20 +0530

    Time: 0 ms (100%), Space: 13.7 MB (78.6%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,20 +1,34 @@
 class Solution(object):
     def removeDuplicates(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        # USING 2 POINTES
+        
+        # s=set(nums)
+        # l=list(s)
+        # l.sort()
+        # for i in range(len(s)):
+        #     nums.insert(i,l[i])
 
-        i=0
-        j=1
+        # return len(l)
+        
+        # length=0
+        # hash=[0]*(len(nums))
+        # for x in nums:
+        #     hash[x]=hash[nums[x]]+1
+        # for i in range(1,len(hash)):
+        #     if hash[i]>0:
+        #         length+=1
+        # return length
 
-        while j<len(nums):
-            if nums[i]!=nums[j]:
-                i+=1
-                nums[i]=nums[j]
-                j+=1
-            elif nums[i]==nums[j]:
-                j+=1
-            
-        return i+1
+        # s=set()
+        # nums=list(s)
+        # # for x in nums:
+        # #     s.add(x)
+        # return len(nums)
+
+        seen=set()
+        k=0
+        for i in range(len(nums)):
+            if nums[i] not in seen:
+                seen.add(nums[i])
+                nums[k]=nums[i]
+                k+=1
+        return k
