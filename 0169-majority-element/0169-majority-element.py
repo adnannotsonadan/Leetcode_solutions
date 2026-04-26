d5b2a9b07b741f477c7b708bf7be3e8081b2ffc7
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-04-26 17:03:14 +0530

    Time: 4 ms (80.08%), Space: 14.5 MB (11.48%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -4,37 +4,8 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-# TAKE EXTRA TIME
-        # m={}
-
-        # for i in range(len(nums)):
-        #     if nums[i] not in m:
-        #         m[nums[i]]=1
-        #     else:
-        #         m[nums[i]]+=1
-        
-        # for x in m:
-        #     if m[x]>(len(nums)//2):
-        #         return x
-# OPTIMAL
-
-        # count=0
-        # num=None
-
-        # for i in range(len(nums)):
-        #     if count==0:
-        #         num=nums[i]
-        #         count+=1
-        #     else:
-        #         if num==nums[i]:
-        #             count+=1
-        #         else:
-        #             count-=1
-        # return num
-        
-
-        num=None
         count=0
+        num=None
 
         for i in range(len(nums)):
             if count==0:
