c340d0fd40c1c021b2af8ec80c4c97977a0fa381
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-04-27 07:47:00 +0530

    Time: 4 ms (19.78%), Space: 13.3 MB (25.75%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -1,3 +1,4 @@
+from collections import deque
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
@@ -10,23 +11,43 @@ class Solution(object):
         :type root: Optional[TreeNode]
         :rtype: List[List[int]]
         """
+        # res=[]
+        # if not root:
+        #     return []
+        # def lvl(node):
+        #     q=deque([])
+        #     q.append(root)
+        #     while q:
+        #         lvl_size=len(q)
+        #         curr_lvl=[]
+        #         for i in range(lvl_size):
+        #             f=q.popleft()
+        #             curr_lvl.append(f.val)
+        #             if f.left is not None:
+        #                 q.append(f.left)
+        #             if f.right is not None:
+        #                 q.append(f.right) 
+        #         res.append(curr_lvl)
+        
+        # lvl(root)
+        # return res
+
         res=[]
         if not root:
-            return []
+            return res
         def lvl(node):
-            q=deque([])
-            q.append(root)
+            q=deque()
+            q.append(node)
             while q:
-                lvl_size=len(q)
-                curr_lvl=[]
-                for i in range(lvl_size):
-                    f=q.popleft()
-                    curr_lvl.append(f.val)
-                    if f.left is not None:
-                        q.append(f.left)
-                    if f.right is not None:
-                        q.append(f.right) 
-                res.append(curr_lvl)
-        
+                lq=len(q)
+                clvl=[]
+                for i in range(lq):
+                    e=q.popleft()
+                    clvl.append(e.val)
+                    if e.left is not None:
+                        q.append(e.left)
+                    if e.right is not None:
+                        q.append(e.right)
+                res.append(clvl[:])
         lvl(root)
         return res
