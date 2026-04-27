e9759bc9598179a2b7049875d6f7f108965e6682
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-04-28 00:13:57 +0530

    Time: 3 ms (11.51%), Space: 12.6 MB (89.47%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
@@ -1,51 +1,61 @@
+from collections import deque
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-from collections import deque
 class Solution(object):
     def zigzagLevelOrder(self, root):
         """
         :type root: Optional[TreeNode]
         :rtype: List[List[int]]
         """
+        # res=[]
         # if not root:
         #     return []
-        # q=deque([root])
-        # res=[]
-        # while q:
-        #     lvl=[]
-        #     for i in range(len(q)):
-        #         f=q.popleft()
-        #         if not f:
-        #             continue
-        #         lvl.append(f.val)
-        #         if f.left:
-        #             q.append(f.left)
-        #         if f.right:
-        #             q.append(f.right)
-        #     res.append(lvl[:])
-        
+        # def lvl(node):
+        #     l_to_r=True
+        #     q=deque([root])
+        #     while q:
+        #         lvl_size=len(q)
+        #         curr_lvl=[]
+        #         for _ in range(lvl_size):
+        #             f=q.popleft()
+        #             curr_lvl.append(f.val)
+        #             if f.left is not None:
+        #                 q.append(f.left)
+        #             if f.right is not None:
+        #                 q.append(f.right)
+        #         if l_to_r:
+        #             res.append(curr_lvl)
+        #             l_to_r=False
+        #         elif not l_to_r:
+        #             res.append(curr_lvl[::-1])
+        #             l_to_r=True
+        # lvl(root)
         # return res
+        
         res=[]
         if not root:
             return []
-        q=deque([root])
-        trav=True
-        while q:
-            lvl=[]
-            for i in range(len(q)):
-                node=q.popleft()
-                lvl.append(node.val)
-                if node.left:
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
-            if trav:
+        def solve(node):
+            flag=False
+            q=deque()
+            q.append(node)
+            while q:
+                lvl=[]
+                for i in range(len(q)):
+                    e=q.popleft()
+                    lvl.append(e.val)
+                    if e.left:
+                        q.append(e.left)
+                    if e.right:
+                        q.append(e.right)
+                if flag==True:
+                    lvl.reverse()
                 res.append(lvl[:])
-            else:
-                res.append(lvl[::-1])
-            trav=not trav
+                flag=not flag
+        solve(root)
         return res
+
