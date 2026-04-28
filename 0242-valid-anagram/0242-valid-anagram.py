86fc028df480fb3df86e387094a20339fc040fb9
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-04-28 10:03:25 +0530

    Time: 27 ms (24.57%), Space: 13.4 MB (41.67%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        # bf
+
+        # return sorted(s)==sorted(t)
+
+        # optimal
+        # m={}
+        # n={}
+
+        # if len(s)!=len(t):
+        #     return False
+        
+        # for i in range(len(s)):
+        #     if s[i] not in m:
+        #         m[s[i]]=1
+        #     else:
+        #         m[s[i]]+=1
+        # for i in range(len(t)):
+        #     if t[i] not in n:
+        #         n[t[i]]=1
+        #     else:
+        #         n[t[i]]+=1
+        
+        # if m==n:
+        #     return True
+        # else:
+        #     return False
+
+        return sorted(s)==sorted(t)
