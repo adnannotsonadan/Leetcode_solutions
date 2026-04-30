41180e3ae197399bf2b177266801a499b468400c
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-05-01 02:00:49 +0530

    Time: 19 ms (66.43%), Space: 14.3 MB (9.36%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,45 +5,26 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        # bf
-
-        # return sorted(s)==sorted(t)
-
-        # optimal
-        # m={}
-        # n={}
-
-        # if len(s)!=len(t):
-        #     return False
-        
-        # for i in range(len(s)):
-        #     if s[i] not in m:
-        #         m[s[i]]=1
-        #     else:
-        #         m[s[i]]+=1
-        # for i in range(len(t)):
-        #     if t[i] not in n:
-        #         n[t[i]]=1
-        #     else:
-        #         n[t[i]]+=1
-        
-        # if m==n:
+        # t="".join(sorted(t))
+        # s="".join(sorted(s))
+        # if s==t:
         #     return True
         # else:
         #     return False
-
-        m={}
+                
+        ms={}
+        mt={}
         if len(s)!=len(t):
             return False
-        for x in s:
-            if x not in m:
-                m[x]=1
+        for i in range(len(s)):
+            if s[i] not in ms:
+                ms[s[i]]=1
             else:
-                m[x]+=1
-        for i in range(len(t)):
-            if t[i] not in m:
-                return False
-            m[t[i]]-=1
-            if m[t[i]]==0:
-                del m[t[i]]
-        return True
+                ms[s[i]]+=1
+            if t[i] not in mt:
+                mt[t[i]]=1
+            else:
+                mt[t[i]]+=1
+        if ms==mt:
+            return True
+        return False
