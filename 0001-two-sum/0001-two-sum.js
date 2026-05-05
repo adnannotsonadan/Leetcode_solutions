7834f9c57a9714f113032674e22f13218d25e6ed
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-05-05 16:44:52 +0530

    Time: 5 ms (43.55%), Space: 57.9 MB (5.97%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let m={};
+    let t=[];
+    for (let i=0;i<nums.length;i++){
+        let x=target-nums[i];
+        if (!(x in m)){
+            m[nums[i]]=i
+        }else{
+            t.push(m[x]);
+            t.push(i);        
+        }
+    }
+    return t;
+};
+//   let map = new Map()
+//   let  n= nums.length
+
+//   for(let i = 0 ; i<n ; i++){
+
+//     let k = target - nums[i];
+
+//     if(map.has(k)){
+//         return [map.get(k) ,i];
+//     }
+
+//     map.set(nums[i] , i)
+//   }
+
+  
+    
