b1002384d2171e8bce08e612d284cae504a23fca
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-05-05 15:24:01 +0530

    Time: 5 ms (30.4%), Space: 67.1 MB (27.33%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    k=k%nums.length;
+    if (nums.length===1){
+        return nums;
+    }
+    reverse(nums,0,nums.length-1);
+    reverse(nums,0,k-1);
+    reverse(nums,k,nums.length-1);
+
+    function reverse(nums,l,r){
+        while (l<r){
+            [nums[l],nums[r]]=[nums[r],nums[l]];
+            l++;
+            r--;
+        }
+    }
+};
