6dee7e6ae072e703abce9bcce18d10aa908f7512
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-05-08 01:12:37 +0530

    Time: 3 ms (53.89%), Space: 13.2 MB (35.65%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,59 +5,38 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        # for i in range(len(nums)):
+        # temp=[]
+        # for i in range(len(nums)-1):
         #     for j in range(i+1,len(nums)):
         #         if nums[i]+nums[j]==target:
-        #             return [i,j]
-
-        # l=0
-        # r=0
-        # res=[]
-        n=len(nums)
-        # s=0
-        # while r<n:
-        #     s+=nums[r]
-        #     while s>target:
-        #         s-=nums[l]
-        #         l+=1
-        #     if s==target:
-        #         break
-        #     r+=1
-        # res.append(l)
-        # res.append(r)
-        # return res
-        # for i in range(n-1):
-        #     for j in range(i+1,n):
-        #         if nums[i]+nums[j]==target:
-        #             return [i,j]
-
-        # i=0
-        # j=0
-        # res=[]
+        #             temp.append(i) 
+        #             temp.append(j) 
+        #         else:
+        #             continue
+        # return temp
+        # n=len(nums)
         # m={}
-        # for i in range(len(nums)):
-        #     if nums[i] not in m:
+        # res=[]
+        # for i in range(n):
+        #     x=target - nums[i]
+        #     if x not in m:
         #         m[nums[i]]=i
         #     else:
-        #         m[nums[i]]=i
-        # for i in range(len(nums)):
-        #     for j in range(i+1,len(nums)):
-        #         if nums[i]+nums[j]==target:
-        #             res.append(i)
-        #             res.append(j)
+        #         res.append(m[x])
+        #         res.append(i)
         # return res
 
-
-        # OPTIMAL APPROACH
-
         m={}
-        n=len(nums)
         res=[]
-        for i in range(n):
-            s=target-nums[i]
-            if s not in m:
+        for i in range(len(nums)):
+            x=target-nums[i]
+            if x not in m:
                 m[nums[i]]=i
             else:
-                res.append(m[s])
+                res.append(m[x])
                 res.append(i)
         return res
+
+            
+                
+        
