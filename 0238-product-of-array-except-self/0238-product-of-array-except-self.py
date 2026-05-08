5d9d837b4ca82551b34ac6ce6b2ea4509a163e2d
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-05-08 22:58:31 +0530

    Time: 37 ms (35.12%), Space: 20.2 MB (72.27%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -16,20 +16,32 @@ class Solution(object):
         #     arr[i]=prod
         # return arr
 
-        n=len(nums)
-        left=[0]*n
-        left[0]=1
-        right=[0]*n
-        right[n-1]=1
+        # n=len(nums)
+        # left=[0]*n
+        # left[0]=1
+        # right=[0]*n
+        # right[n-1]=1
 
+        # for i in range(1,n):
+        #     left[i]=nums[i-1]*left[i-1]
+        
+        # for i in range(n-2,-1,-1):
+        #     right[i]=nums[i+1]*right[i+1]
+        
+        # arr=[0]*n
+        # for i in range(n):
+        #     arr[i]=left[i]*right[i]
+        # return arr
+
+
+        n=len(nums)
+        res=[0]*n
+        res[0]=1
         for i in range(1,n):
-            left[i]=nums[i-1]*left[i-1]
-        
-        for i in range(n-2,-1,-1):
-            right[i]=nums[i+1]*right[i+1]
-        
-        arr=[0]*n
-        for i in range(n):
-            arr[i]=left[i]*right[i]
-        return arr
+            res[i]=res[i-1]*nums[i-1]
+        rp=1
+        for i in range(n-1,-1,-1):
+            res[i]=rp*res[i]
+            rp*=nums[i]
+        return res
 
