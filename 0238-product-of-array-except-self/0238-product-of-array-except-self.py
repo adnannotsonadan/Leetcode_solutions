5972093779245d0c39542b7a5c122ead6ca13ff0
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-05-09 07:44:41 +0530

    Time: 47 ms (21.76%), Space: 22.5 MB (19.24%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -4,44 +4,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        # n=len(nums)
-        # arr=[0]*n
-        # for i in range(n):
-        #     prod=1
-        #     for j in range(n):
-        #         if i==j:
-        #             continue
-        #         else:
-        #             prod*=nums[j]
-        #     arr[i]=prod
-        # return arr
-
-        # n=len(nums)
-        # left=[0]*n
-        # left[0]=1
-        # right=[0]*n
-        # right[n-1]=1
-
-        # for i in range(1,n):
-        #     left[i]=nums[i-1]*left[i-1]
-        
-        # for i in range(n-2,-1,-1):
-        #     right[i]=nums[i+1]*right[i+1]
-        
-        # arr=[0]*n
-        # for i in range(n):
-        #     arr[i]=left[i]*right[i]
-        # return arr
-
-
         n=len(nums)
-        res=[0]*n
-        res[0]=1
-        for i in range(1,n):
-            res[i]=res[i-1]*nums[i-1]
-        rp=1
-        for i in range(n-1,-1,-1):
-            res[i]=rp*res[i]
-            rp*=nums[i]
-        return res
+        left=[0]*n
+        left[0]=1
 
+        right=[0]*n
+        right[n-1]=1
+
+        for i in range(1,n):
+            left[i]=left[i-1]*nums[i-1]
+        for i in range(n-2,-1,-1):
+            right[i]=right[i+1]*nums[i+1]
+        
+        res=[]
+        for i in range(n):
+            res.append(left[i]*right[i])
+        return res
