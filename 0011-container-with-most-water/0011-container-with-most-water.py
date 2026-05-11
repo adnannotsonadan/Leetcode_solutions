546ae41f82b81accbbcd2c7e5f01cd0a2d3604fa
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-05-11 21:19:56 +0530

    Time: 124 ms (25.3%), Space: 20.8 MB (24.94%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -4,27 +4,17 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        # Brute Force
-        # m=float('-inf')
-        # for i in range(len(height)-1):
-        #     for j in range(len(height)-1,i,-1):
-        #         h=min(height[i],height[j])
-        #         w=abs(i-j)
-        #         a=h*w
-        #         if a>m:
-        #             m=a
-        # return m
+        n=len(height)
+        maxi=0
         i=0
-        j=len(height)-1
-        m=0
+        j=n-1
+
         while i<j:
+            width=j-i
             h=min(height[i],height[j])
-            w=abs(j-i)
-            a=h*w
-            if a>m:
-                m=a
-            if height[i]>height[j]:
+            maxi=max(maxi,width*h)
+            if height[i]>=height[j]:
                 j-=1
             else:
                 i+=1
-        return m
+        return maxi
