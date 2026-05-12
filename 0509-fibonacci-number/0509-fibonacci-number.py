05a1d3b375f4a28a474e8cad9d96bf1ab400f41e
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-05-12 16:52:43 +0530

    Time: 536 ms (36.49%), Space: 12.3 MB (88.18%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -4,12 +4,8 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        if n<=1:
-            return n
-        dp=[-1]*(n+1)
-        dp[0]=0
-        dp[1]=1
-
-        for i in range(2,n+1):
-            dp[i]=dp[i-1]+dp[i-2]
-        return dp[n]
+        def rec(n):
+            if n<=1:
+                return n
+            return rec(n-2)+rec(n-1)
+        return rec(n)
