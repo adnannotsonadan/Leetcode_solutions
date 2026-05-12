7233b9795f726e8aab0383fe5dda106289fc6930
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-05-12 16:31:54 +0530

    Time: 15 ms (92.12%), Space: 12.4 MB (98.63%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -4,19 +4,51 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        left=0
-        right=0
-        n=len(s)
-        maxi=0
-        d={}
-        while right<n:
-            if s[right] not in d:
-                d[s[right]]=right
-            else:
-                left=max(left,d[s[right]]+1)
-            d[s[right]]=right
-            maxi=max(maxi,right-left+1)
-            right+=1
-        return maxi
+        # BRUTE FORCE O(n**2)
 
-        
+        # maxi=0
+        # n=len(s)
+        # maxi=0
+        # for i in range(n):
+        #     m=set()
+        #     for j in range(i,n):
+        #         if s[j] not in m:
+        #             m.add(s[j])
+        #         else:
+        #             break
+        #         maxi=max(maxi,j-i+1) 
+        # return maxi
+
+
+        # maxi=0
+        # n=len(s)
+        # i=0
+        # j=0
+        # m={}
+        # while j<n:
+        #     if s[j] not in m:
+        #         m[s[j]]=j
+        #         maxi=max(maxi,j-i+1)
+        #         j+=1
+        #     else:
+        #         i=m[s[j]]+1
+        #         m[s[j]]=j
+        # return maxi
+
+        n=len(s)
+        i=0
+        j=0
+        maxi=0
+        m=set()
+        while j<n:
+            if s[j] not in m:
+                m.add(s[j])
+                j+=1
+            else:
+                while s[j] in m:
+                    m.remove(s[i])
+                    i+=1
+                m.add(s[j])
+                j+=1
+            maxi=max(maxi,j-i)
+        return maxi
