641a6ce399c1856e37c55cbc09ce1b9df94d4c01
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-05-13 12:44:58 +0530

    Time: 0 ms (100%), Space: 12.3 MB (56.81%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -11,24 +11,71 @@ class Solution(object):
         #     nums1[i]=nums2[i-m]
         # nums1.sort()
 
-        temp=[]
+        # temp=[]
+        # i=0
+        # j=0
+        # while i<m and j<n :
+        #     if nums1[i]<nums2[j]:
+        #         temp.append(nums1[i])
+        #         i+=1
+        #     elif nums1[i]>nums2[j]:
+        #         temp.append(nums2[j])
+        #         j+=1
+        #     elif nums1[i]==nums2[j]:
+        #         temp.append(nums1[i])
+        #         i+=1
+        # while i < m:
+        #     temp.append(nums1[i])
+        #     i+=1
+        # while j < n:
+        #     temp.append(nums2[j])
+        #     j+=1
+        # nums1[:]=temp
+        # return nums1
+
+
+        # NO EXTRA SPACE APPROACH
+
+        # i=m-1   #nums1
+        # j=n-1  #nums2
+        # k=m+n-1
+
+        # if m==0:
+        #     nums1[:]=nums2
+        # while i>=0 and j>=0:
+        #     if nums2[j]>nums1[i]:
+        #         nums1[k]=nums2[j]
+        #         j-=1
+        #         k-=1
+        #     elif nums2[j]<nums1[i]:
+        #         nums1[k]=nums1[i]
+        #         i-=1
+        #         k-=1
+        #     elif nums2[j]==nums1[i]:
+        #         nums1[k]=nums1[i]
+        #         i-=1
+        #         k-=1
+        # while j>=0:
+        #     nums1[k]=nums2[j]
+        #     j-=1
+        #     k-=1
+        # return nums1
+
         i=0
         j=0
-        while i<m and j<n :
-            if nums1[i]<nums2[j]:
-                temp.append(nums1[i])
+        k=[]
+        while i!=m and j!=n:
+            if nums1[i]<=nums2[j]:
+                k.append(nums1[i])
                 i+=1
-            elif nums1[i]>nums2[j]:
-                temp.append(nums2[j])
+            else:
+                k.append(nums2[j])
                 j+=1
-            elif nums1[i]==nums2[j]:
-                temp.append(nums1[i])
-                i+=1
-        while i < m:
-            temp.append(nums1[i])
+        while i<m:
+            k.append(nums1[i])
             i+=1
-        while j < n:
-            temp.append(nums2[j])
+        
+        while j<n:
+            k.append(nums2[j])
             j+=1
-        nums1[:]=temp
-        return nums1
+        nums1[:]=k
