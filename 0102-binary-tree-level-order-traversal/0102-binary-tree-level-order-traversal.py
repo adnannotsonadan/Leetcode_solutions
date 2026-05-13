c2d22da0a8286aa364d80b5d2394f80c7f825985
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-05-13 19:54:10 +0530

    Time: 2 ms (44.62%), Space: 13.1 MB (55.69%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -11,43 +11,19 @@ class Solution(object):
         :type root: Optional[TreeNode]
         :rtype: List[List[int]]
         """
-        # res=[]
-        # if not root:
-        #     return []
-        # def lvl(node):
-        #     q=deque([])
-        #     q.append(root)
-        #     while q:
-        #         lvl_size=len(q)
-        #         curr_lvl=[]
-        #         for i in range(lvl_size):
-        #             f=q.popleft()
-        #             curr_lvl.append(f.val)
-        #             if f.left is not None:
-        #                 q.append(f.left)
-        #             if f.right is not None:
-        #                 q.append(f.right) 
-        #         res.append(curr_lvl)
-        
-        # lvl(root)
-        # return res
-
-        res=[]
         if not root:
-            return res
-        def lvl(node):
-            q=deque()
-            q.append(node)
-            while q:
-                lq=len(q)
-                clvl=[]
-                for i in range(lq):
-                    e=q.popleft()
-                    clvl.append(e.val)
-                    if e.left is not None:
-                        q.append(e.left)
-                    if e.right is not None:
-                        q.append(e.right)
-                res.append(clvl[:])
-        lvl(root)
+            return []
+        res=[]
+        q=deque([])
+        q.append(root)
+        while q:
+            temp=[]
+            for i in range(len(q)):
+                f=q.popleft()
+                temp.append(f.val)
+                if f.left is not None:
+                    q.append(f.left)
+                if f.right is not None:
+                    q.append(f.right)
+            res.append(temp[:])
         return res
