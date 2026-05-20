c47503b6160225fda88db1ad58ff870aa100c421
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-05-21 05:21:46 +0530

    Time: 6 ms (53.67%), Space: 12.3 MB (57.75%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -5,57 +5,27 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
-        # n=len(grid)
-        # m=len(grid[0])
-        # vis=[[0]*m for _ in range(n)]
-        # tm=0
-        # moves=[(1,0),(0,1),(-1,0),(0,-1)]
-        # q=deque()
-        # for i in range(n):
-        #     for j in range(m):
-        #         if grid[i][j]==2:
-        #             q.append((i,j,0))    
-        
-        # while q:
-        #     f1,f2,t=q.popleft()
-        #     tm=max(tm,t)
-        #     for di,dj in moves:
-        #         ni=f1+di
-        #         nj=f2+dj
-        #         if ni>=0 and ni<n and nj>=0 and nj<m and grid[ni][nj]==1:
-        #             if vis[ni][nj]==0:
-        #                 vis[ni][nj]=1
-        #                 grid[ni][nj]=2
-        #                 q.append((ni,nj,t+1))
-        # for i in range(n):
-        #     for j in range(m):
-        #         if grid[i][j]==1:
-        #             return -1
-        # return tm
-
         n=len(grid)
         m=len(grid[0])
-        q=deque()
-        moves=[(1,0),(0,1),(-1,0),(0,-1)]
         vis=[[0]*m for _ in range(n)]
+        q=deque()
+        moves=[(1,0),(-1,0),(0,1),(0,-1)]
         for i in range(n):
             for j in range(m):
                 if grid[i][j]==2:
                     q.append((i,j,0))
                     vis[i][j]=1
-                    
-        tm=0       
+        tm=0
         while q:
-            dx,dy,t=q.popleft()
-            tm=max(t,tm)
-            for f,l in moves:
-                ni=f+dx
-                nj=l+dy
+            f,l,t=q.popleft()
+            tm=max(tm,t)
+            for dx,dy in moves:
+                ni=dx+f
+                nj=dy+l
                 if ni>=0 and ni<n and nj>=0 and nj<m and grid[ni][nj]==1:
-                    if not vis[ni][nj]:
-                        vis[ni][nj]=1
-                        q.append((ni,nj,t+1))
-                        grid[ni][nj]=2
+                    q.append((ni,nj,t+1))
+                    vis[ni][nj]=1
+                    grid[ni][nj]=2
         for i in range(n):
             for j in range(m):
                 if grid[i][j]==1:
