31c2bcf032b1c53c93e4794d73b86f96d4d2b637
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-05-21 04:43:18 +0530

    Time: 15 ms (24.93%), Space: 13.1 MB (64.14%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -4,103 +4,26 @@ class Solution(object):
         :type isConnected: List[List[int]]
         :rtype: int
         """
-        # n=len(isConnected)
-        # adj=[[] for _ in range(n)]
-        # for i in range(n):
-        #     for j in range(n):
-        #         if isConnected[i][j]==1:
-        #             adj[i].append(j)
-        #             adj[j].append(i)
-        # def dfs(i):
-        #     vis[i]=1
-        #     for nei in adj[i]:
-        #         if vis[nei]==0:
-        #             dfs(nei)
-            
-        # vis=[0]*(n)
-        # l=0
-        # for i in range(n):
-        #     if vis[i]==0:
-        #         l+=1
-        #         dfs(i)
-        # return l
-#         n=len(isConnected)
-        
-#         dsu=DSU(n)
-#         for i in range(n):
-#             for j in range(n):
-#                 if isConnected[i][j]==1:
-#                     dsu.unite(i,j)
-#         return dsu.components
-
-# class DSU:
-#     def __init__(self,n):
-#         self.parent=[i for i in range(n)]
-#         self.size=[1]*(n)
-#         self.components=n
-#     def find(self,x):
-#         if x==self.parent[x]:
-#             return x
-#         self.parent[x]=self.find(self.parent[x])
-#         return self.parent[x]
-#     def unite(self,u,v):
-#         u=self.find(u)
-#         v=self.find(v)
-
-#         if u==v:
-#             return False
-#         if self.size[u]<self.size[v]:
-#             u,v=v,u
-#         self.parent[v]=u
-#         self.size[u]+=self.size[v]
-#         self.components-=1
-#         return True
-
-
-
-        # n=len(isConnected)
-        # adj=[[] for _ in range(n)]
-        # for i in range(n):
-        #     for j in range(n):
-        #         if isConnected[i][j]==1 and i!=j:
-        #             adj[i].append(j)
-        #             adj[j].append(i)
-
-
-        # vis=[0]*n
-
-        # def dfs(src):
-        #     vis[src]=1
-        #     for nei in adj[src]:
-        #         if not vis[nei]:
-        #             dfs(nei)
-        # cc=0
-        # for i in range(n):
-        #     if not vis[i]:
-        #         cc+=1
-        #         dfs(i)
-        # return cc
-
         n=len(isConnected)
         m=len(isConnected[0])
-        adj=[[]*n for _ in range(n)]
-        vis=[0]*n
+
+        adj=[[] for _ in range(n)]
 
         for i in range(n):
             for j in range(m):
-                if isConnected[i][j]==1 and i!=j:
+                if isConnected[i][j]==1:
                     adj[i].append(j)
                     adj[j].append(i)
         
+        vis=[0]*n
         def dfs(node):
             vis[node]=1
             for nei in adj[node]:
-                if vis[nei]==0:
+                if not vis[nei]:
                     dfs(nei)
-        
-        cc=0
+        c=0
         for i in range(n):
-            if vis[i]==0:
-                cc+=1
+            if not vis[i]:
+                c+=1
                 dfs(i)
-        return cc
+        return c
