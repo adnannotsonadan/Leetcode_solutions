50f8a6c75dcb040b2495304e069c94a6086ff113
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-06-15 20:20:22 +0530

    Time: 265 ms (38.41%), Space: 69.3 MB (8.39%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -10,63 +10,14 @@ class Solution(object):
         :type head1, head1: ListNode
         :rtype: ListNode
         """
-        # temp1=headA
-        # temp2=headB
-        # l1={}
-        # l2={}
-        # while temp1:
-        #     l1[temp1]=1
-        #     temp1=temp1.next
-            
-        # while temp2:
-        #     if temp2 in l1:
-        #         return temp2
-        #     else:
-        #         temp2=temp2.next
-        # sol2
-
-        temp1=headA
-        temp2=headB
-        l1=0
+        s=set()
+        temp=headA
+        while temp:
+            s.add(temp)
+            temp=temp.next
+        temp1=headB
         while temp1:
-            l1+=1
+            if temp1 in s:
+                return temp1
             temp1=temp1.next
-        l2=0
-        while temp2:
-            l2+=1
-            temp2=temp2.next
-        diff=abs(l1-l2)
-
-        temp1=headA
-        temp2=headB
-        if l1>l2:
-            while diff!=0:
-                diff-=1
-                temp1=temp1.next
-            while temp1 or temp2:
-                if temp1==temp2:
-                    return temp1
-                else:
-                    temp1=temp1.next
-                    temp2=temp2.next
-            
-        elif l2>l1:
-            while diff!=0:
-                diff-=1
-                temp2=temp2.next
-            while temp1 or temp2:
-                if temp1==temp2:
-                    return temp1
-                else:
-                    temp1=temp1.next
-                    temp2=temp2.next
-                    
-        if l1==l2:
-            while temp1 or temp2:
-                if temp1==temp2:
-                    return temp1
-                else:
-                    temp1=temp1.next
-                    temp2=temp2.next
         return None
-
