b8ce3c698582d09b024a0f0203bdaecc741546a1
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-06-16 10:37:26 +0530

    Time: 0 ms (100%), Space: 12.6 MB (9.96%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -5,32 +5,16 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        dp=[[0]*n for _ in range(m)]
-        # def solve(i,j):
-        #     if i==m-1 and j==n-1:
-        #         return 1
-        #     if  i>=m or j>=n:
-        #         return 0
-        #     if dp[i][j]!=0:
-        #         return dp[i][j]
-        #     down=solve(i+1,j)
-        #     right=solve(i,j+1)
-        #     dp[i][j] = down+right
-        #     return dp[i][j]
-        # return solve(0,0)
-
-        dp[0][0]=1
-        for i in range(m):
-            for j in range(n):
-                if i==0 and j==0:
-                    continue
-                if i>0:
-                    down=dp[i-1][j]
-                else:
-                    down=0
-                if j>0:
-                    right=dp[i][j-1]
-                else:
-                    right=0
-                dp[i][j]=down+right
-        return dp[m-1][n-1]
+        dp=[[-1]*n for _ in range(m)]
+        def rec(i,j):
+            if i<0 or j<0:
+                return 0
+            if i==0 and j==0:
+                return 1
+            if dp[i][j]!=-1:
+                return dp[i][j]
+            left=rec(i-1,j)
+            up=rec(i,j-1)
+            dp[i][j] = up+left
+            return dp[i][j]
+        return rec(m-1,n-1)
