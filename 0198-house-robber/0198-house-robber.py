30c1e293e3010b74c07cbc5312b468efcc692029
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-06-16 11:50:13 +0530

    Time: 0 ms (100%), Space: 12.3 MB (91%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -4,14 +4,25 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        # n=len(nums)
+        # dp=[-1]*n
+        # dp[0]=nums[0]
+        # for i in range(1,n):
+        #     if i>1:
+        #         pick=nums[i]+dp[i-2]
+        #     else:
+        #         pick=nums[i]
+        #     np=dp[i-1]
+        #     dp[i]=max(pick,np)
+        # return dp[n-1]
+
         n=len(nums)
         dp=[-1]*n
         dp[0]=nums[0]
         for i in range(1,n):
+            pick=nums[i]
             if i>1:
-                pick=nums[i]+dp[i-2]
-            else:
-                pick=nums[i]
-            np=dp[i-1]
-            dp[i]=max(pick,np)
+                pick+=dp[i-2]
+            not_pick=0+dp[i-1]
+            dp[i]=max(pick,not_pick)
         return dp[n-1]
