646ed172267a9ed24596c6ec48ac4015da0aafb2
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-06-16 19:13:17 +0530

    Time: 5 ms (84.62%), Space: 18.1 MB (84.62%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def processStr(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        res=''
+        for char in s:
+            if char.isalpha():
+                res+=char
+            elif char=='*':
+                res=res[:-1]
+            elif char=='#':
+                res+=res
+            elif char=='%':
+                res=res[::-1]  
+        return res
