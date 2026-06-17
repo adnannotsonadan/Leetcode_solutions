8cb5c9d4276f5e869d4cfc547f74e1a28edcffd5
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-06-17 18:40:33 +0530

    Time: 8 ms (55.72%), Space: 17.7 MB (56.49%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -5,8 +5,8 @@ class Solution(object):
         :rtype: List[int]
         """
         res=[0]*(n+1)
-
-        for i in range(n+1):
+        res[0]=0
+        for i in range(1,n+1):
             if i%2==1:
                 res[i]=res[i//2]+1
             else:
