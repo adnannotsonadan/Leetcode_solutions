ce58feba5d48932ae7b6817393bec959c863796e
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-07-02 18:18:44 +0530

    Time: 0 ms (100%), Space: 12.6 MB (62.11%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -7,20 +7,45 @@ class Solution(object):
         :type color: int
         :rtype: List[List[int]]
         """
-        n=len(image)
+        # n=len(image)
+        # m = len(image[0])
+        # clr=image[sr][sc]
+        # if clr==color:
+        #     return image
+        # def dfs(i,j):
+        #     image[i][j]=color
+        #     if i>0 and image[i-1][j]==clr:
+        #         dfs(i-1,j)
+        #     if i<n-1 and image[i+1][j]==clr:
+        #         dfs(i+1,j)
+        #     if j>0 and image[i][j-1]==clr:
+        #         dfs(i,j-1)
+        #     if j<m-1 and image[i][j+1]==clr:
+        #         dfs(i,j+1)
+        # dfs(sr,sc)
+        # return image
+
+
+        n = len(image)
         m = len(image[0])
-        clr=image[sr][sc]
-        if clr==color:
+
+        clr = image[sr][sc]
+
+        if clr == color:
             return image
-        def dfs(i,j):
-            image[i][j]=color
-            if i>0 and image[i-1][j]==clr:
-                dfs(i-1,j)
-            if i<n-1 and image[i+1][j]==clr:
-                dfs(i+1,j)
-            if j>0 and image[i][j-1]==clr:
-                dfs(i,j-1)
-            if j<m-1 and image[i][j+1]==clr:
-                dfs(i,j+1)
-        dfs(sr,sc)
+
+        moves = [(-1, 0), (1, 0), (0, -1), (0, 1)]   # Up, Down, Left, Right
+
+        def dfs(i, j):
+            image[i][j] = color
+
+            for dr, dc in moves:
+                nr = i + dr
+                nc = j + dc
+
+                if 0 <= nr < n and 0 <= nc < m and image[nr][nc] == clr:
+                    dfs(nr, nc)
+
+        dfs(sr, sc)
+
         return image
