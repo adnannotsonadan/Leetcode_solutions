98ce504fbd23e53abe02b0a5b368874a9a011634
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-07-16 23:58:55 +0530

    Time: 3 ms (8.71%), Space: 19.4 MB (28.73%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,13 +1,20 @@
-class Solution(object):
-    def generate(self, numRows):
-        """
-        :type numRows: int
-        :rtype: List[List[int]]
-        """
-        tri=[]
-        for i in range(numRows):
-            row=[1]*(i+1)
-            for j in range(1,i):
-                row[j]=tri[i-1][j]+tri[i-1][j-1]
-            tri.append(row)
-        return tri        
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        matrix=[]
+        matrix.append([1])
+        matrix.append([1,1])
+        if numRows==1:
+            return [matrix[0]]
+        if numRows==2:
+            return [matrix[0],matrix[1]]
+        for i in range(3,numRows+1):
+            res=[]
+            for j in range(0,i):
+                if j==0 or j==i-1:
+                    res.append(1)
+                elif 0<j<i-1:
+                    res.append(matrix[i-2][j-1]+matrix[i-2][j])
+                    
+            matrix.append(res[:])
+        return matrix
+        
