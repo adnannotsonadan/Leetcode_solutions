dcc77270e66453bd0afa2f9ccaef98e9e5ba27fa
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-07-24 15:49:38 +0530

    Time: 15 ms (41.23%), Space: 19.2 MB (68.15%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,54 +1,34 @@
-class Solution(object):
-    def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        # BRUTE FORCE O(n**2)
-
-        # maxi=0
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
         # n=len(s)
         # maxi=0
+        
         # for i in range(n):
         #     m=set()
         #     for j in range(i,n):
         #         if s[j] not in m:
         #             m.add(s[j])
+        #             maxi=max(maxi,len(m))
         #         else:
         #             break
-        #         maxi=max(maxi,j-i+1) 
         # return maxi
 
 
-        # maxi=0
-        # n=len(s)
-        # i=0
-        # j=0
-        # m={}
-        # while j<n:
-        #     if s[j] not in m:
-        #         m[s[j]]=j
-        #         maxi=max(maxi,j-i+1)
-        #         j+=1
-        #     else:
-        #         i=m[s[j]]+1
-        #         m[s[j]]=j
-        # return maxi
-
-        n=len(s)
+        maxi=0
         i=0
         j=0
-        maxi=0
+        n=len(s)
         m=set()
-        while j<n:
-            if s[j] not in m:
-                m.add(s[j])
-                j+=1
+        while i<n:
+            if j<n:
+                if s[j] not in m:
+                    m.add(s[j])
+                    maxi=max(maxi,j-i+1)
+                    j+=1
+                else:
+                    while s[j] in m:
+                        m.remove(s[i])
+                        i+=1
             else:
-                while s[j] in m:
-                    m.remove(s[i])
-                    i+=1
-                m.add(s[j])
-                j+=1
-            maxi=max(maxi,j-i)
+                i+=1
         return maxi
