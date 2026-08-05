1a97137027e651219080eb3d7eb483aa0bd6c78f
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-08-05 15:18:20 +0530

    Time: 0 ms (100%), Space: 19.4 MB (77.24%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,31 +1,21 @@
-class Solution(object):
-    def search(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        s = 0
-        e = len(nums) - 1
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        n=len(nums)
+        l=0
+        h=n-1
         
-        while s <= e:
-            mid = (s + e) // 2
-            
-            if nums[mid] == target:
+        while l<=h:
+            mid=(l+h)//2
+            if nums[mid]==target:
                 return mid
-            
-            # left half sorted
-            if nums[s] <= nums[mid]:
-                if nums[s] <= target < nums[mid]:
-                    e = mid - 1
+            elif nums[mid]>=nums[l]:
+                if target>=nums[l] and target<=nums[mid]:
+                    h=mid-1
                 else:
-                    s = mid + 1
-            
-            # right half sorted
+                    l=mid+1
             else:
-                if nums[mid] < target <= nums[e]:
-                    s = mid + 1
+                if target>=nums[mid] and target<=nums[h]:
+                    l=mid+1
                 else:
-                    e = mid - 1
-        
+                    h=mid-1
         return -1
