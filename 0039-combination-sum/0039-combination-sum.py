2138c5a6ac51e56ff06d2df56b3203af5a111bb2
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-08-09 12:49:30 +0530

    Time: 7 ms (76.6%), Space: 19.6 MB (64.82%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,20 +1,17 @@
-class Solution(object):
-    def combinationSum(self, candidates, target):
-        """
-        :type candidates: List[int]
-        :type target: int
-        :rtype: List[List[int]]
-        """
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res=[]
-        def rec(ind,arr,target):
+
+        def rec(ind,ans,tot):
             if ind==len(candidates):
-                if target==0:
-                    res.append(arr[:])
+                if tot==0:
+                    res.append(ans[:])
                 return
-            if target>0:
-                arr.append(candidates[ind])
-                rec(ind,arr,target-candidates[ind])
-                arr.pop()
-            rec(ind+1,arr,target)
+            
+            if candidates[ind]<=tot:
+                ans.append(candidates[ind])
+                rec(ind,ans,tot-candidates[ind])
+                ans.pop()
+            rec(ind+1,ans,tot)
         rec(0,[],target)
         return res
